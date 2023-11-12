16fb332a3f28c43bfc938ca9c9e6b947dfe8acd1
hsimyu <hsimyu1900@gmail.com>
2023-11-12 23:46:42 +0900

    Impl binary operation.

--- cpplox/main.cpp
+++ cpplox/main.cpp
@@ -10,9 +10,26 @@ int main(int argc, const char* argv[])
 
 	// 定数プールに値を格納し、そのインデックスを取得
 	// それを定数命令とそのオペランドとしてチャンクに格納する
+
 	int constant = c.AddConstant(1.2);
 	c.Write(OP_CONSTANT, 123);
 	c.Write(constant, 123);
+
+	constant = c.AddConstant(3.4);
+	c.Write(OP_CONSTANT, 123);
+	c.Write(constant, 123);
+
+	// 1.2 + 3.4
+	c.Write(OP_ADD, 123);
+
+	constant = c.AddConstant(4.6);
+	c.Write(OP_CONSTANT, 123);
+	c.Write(constant, 123);
+
+	// (1.2 + 3.4) / 4.6
+	c.Write(OP_DIVIDE, 123);
+
+	// -1.0
 	c.Write(OP_NEGATE, 123);
 
 	c.Write(OP_RETURN, 123);
--- cpplox/vm.cpp
+++ cpplox/vm.cpp
@@ -18,6 +18,13 @@ Value read_constant()
 	return (vm.chunk->constants.values[read_byte()]);
 }
 
+#define BINARY_OP(op) \
+	do { \
+		double b = pop(); \
+		double a = pop(); \
+		push(a op b); \
+	} while (false)
+
 InterpretResult run()
 {
 	for (;;)
@@ -45,6 +52,11 @@ InterpretResult run()
 			break;
 		}
 
+		case OP_ADD: BINARY_OP(+); break;
+		case OP_SUBTRACT: BINARY_OP(-); break;
+		case OP_MULTIPLY: BINARY_OP(*); break;
+		case OP_DIVIDE: BINARY_OP(/); break;
+
 		case OP_NEGATE: {
 			// push し直すより直接書き換えた方が速そう…?
 			push(-pop());
@@ -63,6 +75,8 @@ InterpretResult run()
 		}
 	}
 }
+
+#undef BINARY_OP
 
 void resetStack()
 {

f471d6df9fa327480a6cb7e6aa17a6c47621e696
hsimyu <hsimyu1900@gmail.com>
2023-11-13 22:57:52 +0900

    Impl scanner.

--- cpplox/compiler.cpp
+++ cpplox/compiler.cpp
@@ -2,7 +2,26 @@
 
 #include "scanner.h"
 
+#include <cstdio>
+
 void compile(const char* source)
 {
 	initScanner(source);
+	int line = -1;
+
+	for (;;) {
+		Token token = scanToken();
+		if (token.line != line)
+		{
+			printf("%04d ", token.line);
+			line = token.line;
+		}
+		else
+		{
+			printf(" | ");
+		}
+		printf("%02d '%.*s'\n", token.type, token.length, token.start);
+
+		if (token.type == TOKEN_EOF) break;
+	}
 }
--- cpplox/scanner.cpp
+++ cpplox/scanner.cpp
@@ -18,6 +18,20 @@ bool isAtEnd()
 	return *scanner.current == '\0';
 }
 
+char advance()
+{
+	scanner.current++;
+	return scanner.current[-1]; // current の一つ前を返す
+}
+
+bool match(char expected)
+{
+	if (isAtEnd()) return false;
+	if (*scanner.current != expected) return false;
+	scanner.current++;
+	return true;
+}
+
 Token makeToken(TokenType type)
 {
 	return {
@@ -52,6 +66,30 @@ Token scanToken()
 	scanner.start = scanner.current;
 
 	if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+	char c = advance();
+	switch (c)
+	{
+		case '(': return makeToken(TOKEN_LEFT_PAREN);
+		case ')': return makeToken(TOKEN_RIGHT_PAREN);
+		case '{': return makeToken(TOKEN_LEFT_BRACE);
+		case '}': return makeToken(TOKEN_RIGHT_BRACE);
+		case ';': return makeToken(TOKEN_SEMICOLON);
+		case ',': return makeToken(TOKEN_COMMA);
+		case '.': return makeToken(TOKEN_DOT);
+		case '-': return makeToken(TOKEN_MINUS);
+		case '+': return makeToken(TOKEN_PLUS);
+		case '/': return makeToken(TOKEN_SLASH);
+		case '*': return makeToken(TOKEN_STAR);
+		case '!':
+			return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+		case '=':
+			return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+		case '<':
+			return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+		case '>':
+			return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+	}
 
 	return errorToken("Unexpected character");
 }

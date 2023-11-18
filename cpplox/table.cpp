2fd4c06118b629e7a45390ee9ccb14f44e9bf61c
hsimyu <hsimyu1900@gmail.com>
2023-11-18 15:27:47 +0900

    Define table type.

---
+++ cpplox/table.cpp
@@ -0,0 +1,17 @@
+﻿#include "table.h"
+
+#include "memory.h"
+
+void initTable(Table* table)
+{
+	table->count = 0;
+	table->capacity = 0;
+	table->entries = nullptr;
+}
+
+void freeTable(Table* table)
+{
+	free_array(table->entries, table->capacity);
+	initTable(table);
+}
+
---
+++ cpplox/table.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "object.h"
+
+struct Entry {
+	ObjString* key = nullptr;
+	Value value;
+};
+
+struct Table {
+	int count = 0;
+	int capacity = 0;
+	Entry* entries = nullptr;
+};
+
+void initTable(Table* table);
+void freeTable(Table* table);

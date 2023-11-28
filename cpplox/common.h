917c9958433bdc43f58dd46ef12df9682ab252e7
hsimyu <hsimyu1900@gmail.com>
2023-11-28 23:35:09 +0900

    Add fib() test.

--- cpplox/common.h
+++ cpplox/common.h
@@ -1,7 +1,13 @@
 ﻿#pragma once
 
+#if _DEBUG
 #define DEBUG_PRINT_CODE 1
 #define DEBUG_TRACE_EXECUTION 1
+#else
+#define DEBUG_PRINT_CODE 0
+#define DEBUG_TRACE_EXECUTION 0
+#endif
+
 #define LOCAL_VARIABLE_COUNT (UINT8_MAX + 1)
 
 #include "chunk.h"

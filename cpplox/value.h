fe0b664950b846669b990d6778b93d9f3e0173d9
hsimyu <hsimyu1900@gmail.com>
2023-12-12 23:22:00 +0900

    Prepare NaN-Boxing.

--- cpplox/value.h
+++ cpplox/value.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include "common.h"
+
 struct Obj;
 struct ObjClass;
 struct ObjInstance;
@@ -10,12 +12,65 @@ struct ObjClosure;
 struct ObjUpvalue;
 struct ObjString;
 
+#if NAN_BOXING
+
+#include <cstring>
+
+#define QUEIT_NAN (static_cast<uint64_t>(0x7ffc'0000'0000'0000))
+
+#define TAG_NIL 1
+#define TAG_FALSE 2
+#define TAG_TRUE 3
+
+using Value = uint64_t;
+
+#define IS_NUMBER(value) (((value) & QUEIT_NAN) != QNAN)
+#define AS_NUMBER(value) valueToNum(value)
+#define TO_NUMBER(value) numToValue(value)
+
+#define NIL_VAL (static_cast<Value>(QUEIT_NAN | TAG_NIL))
+#define FALSE_VAL (static_cast<Value>(QUEIT_NAN | TAG_FALSE))
+#define TRUE_VAL (static_cast<Value>(QUEIT_NAN | TAG_TRUE))
+
+#define TO_NIL() NIL_VAL
+#define IS_NIL(value) ((value) == NIL_VAL)
+
+#define IS_BOOL(value) (((value) | 1) == TRUE_VAL)
+#define AS_BOOL(value) ((value) == TRUE_VAL)
+#define TO_BOOL(value) ((value) ? TRUE_VAL : FALSE_VAL)
+
+inline Value numToValue(double num)
+{
+	Value value;
+	memcpy(&value, &num, sizeof(double));
+	return value;
+}
+
+inline double valueToNum(Value value)
+{
+	double num;
+	memcpy(&num, &value, sizeof(Value));
+	return num;
+}
+
+#else
+
 enum class ValueType
 {
 	Bool,
 	Nil,
 	Number,
 	Obj,
+};
+
+struct Value
+{
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+		Obj* obj;
+	} as;
 };
 
 #define IS_BOOL(value) ((value).type == ValueType::Bool)
@@ -31,16 +86,6 @@ enum class ValueType
 #define TO_NIL() Value{ValueType::Nil, {.number = 0} }
 #define TO_NUMBER(value) Value{ValueType::Number, {.number = value} }
 #define TO_OBJ(value) Value{ValueType::Obj, {.obj = reinterpret_cast<Obj*>(value)} }
-
-struct Value
-{
-	ValueType type;
-	union {
-		bool boolean;
-		double number;
-		Obj* obj;
-	} as;
-};
 
 #endif
 

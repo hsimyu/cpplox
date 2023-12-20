8141a22b9b2f49c0dcc6f709e5e9b6bec51375d9
hsimyu <hsimyu1900@gmail.com>
2023-12-20 23:18:09 +0900

    Split thread.h.

---
+++ cpplox/thread.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "value.h"
+
+constexpr size_t FRAMES_MAX = 64;
+constexpr size_t STACK_COUNT_MAX = 1024;
+
+struct ObjClosure;
+struct ObjUpvalue;
+
+struct CallFrame
+{
+	ObjClosure* closure = nullptr;
+	uint8_t* ip = nullptr;
+	Value* slots = nullptr;
+};
+
+struct Thread
+{
+	// 関数スタック
+	CallFrame frames[FRAMES_MAX] = { };
+	int frameCount = 0;
+
+	Value stack[STACK_COUNT_MAX] = { };
+	Value* stackTop = nullptr;
+
+	ObjUpvalue* openUpvalues = nullptr;
+};
+void initThread(Thread* thread);
+void freeThread(Thread* thread);
+
--- cpplox/vm.h
+++ cpplox/vm.h
@@ -3,34 +3,10 @@
 #include "chunk.h"
 #include "value.h"
 #include "table.h"
-
-constexpr size_t FRAMES_MAX = 64;
-constexpr size_t STACK_COUNT_MAX = 1024;
+#include "thread.h"
 
 struct Obj;
 struct ObjClosure;
-struct ObjUpvalue;
-
-struct CallFrame
-{
-	ObjClosure* closure = nullptr;
-	uint8_t* ip = nullptr;
-	Value* slots = nullptr;
-};
-
-struct Thread
-{
-	// 関数スタック
-	CallFrame frames[FRAMES_MAX] = { };
-	int frameCount = 0;
-
-	Value stack[STACK_COUNT_MAX] = { };
-	Value* stackTop = nullptr;
-
-	ObjUpvalue* openUpvalues = nullptr;
-};
-void initThread(Thread* thread);
-void freeThread(Thread* thread);
 
 struct VM
 {
